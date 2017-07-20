c4f4305fe6f3216492234551224559936e3604f6
Todd Cope <xander4jesus@yahoo.com>
2017-07-20 01:20:24 -0500

    Make Play Files, Queue Files, Play Folder, and Queue Folder menu options handle all files and archives correctly.

--- src/ui_menu_proc.c
+++ src/ui_menu_proc.c
@@ -108,15 +108,54 @@ static int omo_get_total_files(ALLEGRO_FILECHOOSER * fc, void * data)
     return total_files;
 }
 
+static void add_files_to_queue(ALLEGRO_FILECHOOSER * fc, OMO_QUEUE * queue, void * data)
+{
+    APP_INSTANCE * app = (APP_INSTANCE *)data;
+    OMO_ARCHIVE_HANDLER * archive_handler = NULL;
+    OMO_PLAYER * player;
+    char buf[256];
+    int i, j, c;
+
+    for(i = 0; i < al_get_native_file_dialog_count(fc); i++)
+    {
+        /* add all archived files if we have an archive handler for this file type */
+        archive_handler = omo_get_archive_handler(app->archive_handler_registry, al_get_native_file_dialog_path(fc, i));
+        if(archive_handler)
+        {
+            c = archive_handler->count_files(al_get_native_file_dialog_path(fc, i));
+            for(j = 0; j < c; j++)
+            {
+                player = omo_get_player(app->player_registry, archive_handler->get_file(al_get_native_file_dialog_path(fc, i), j));
+                if(player)
+                {
+                    sprintf(buf, "%d", j);
+                    omo_add_file_to_queue(queue, al_get_native_file_dialog_path(fc, i),  buf);
+                }
+            }
+        }
+
+        /* otherwise, add single file */
+        else
+        {
+            player = omo_get_player(app->player_registry, al_get_native_file_dialog_path(fc, i));
+            if(player)
+            {
+                c = player->get_track_count(al_get_native_file_dialog_path(fc, i));
+                for(j = 0; j < c; j++)
+                {
+                    sprintf(buf, "%d", j);
+                    omo_add_file_to_queue(queue, al_get_native_file_dialog_path(fc, i), c > 1 ? buf : NULL);
+                }
+            }
+        }
+    }
+}
+
 int omo_menu_file_play_files(void * data)
 {
     APP_INSTANCE * app = (APP_INSTANCE *)data;
     ALLEGRO_FILECHOOSER * fc;
-    OMO_ARCHIVE_HANDLER * archive_handler = NULL;
-    OMO_PLAYER * player;
     int total_files = 0;
-    int i, j, c;
-    char buf[256];
 
 	fc = al_create_native_file_dialog(app->last_music_filename, "Select music files.", omo_get_type_string(data), ALLEGRO_FILECHOOSER_FILE_MUST_EXIST | ALLEGRO_FILECHOOSER_MULTIPLE);
 	if(!fc)
@@ -140,38 +179,7 @@ int omo_menu_file_play_files(void * data)
     app->queue = omo_create_queue(total_files);
     if(app->queue)
     {
-        for(i = 0; i < al_get_native_file_dialog_count(fc); i++)
-        {
-            /* add all archived files if we have an archive handler for this file type */
-            archive_handler = omo_get_archive_handler(app->archive_handler_registry, al_get_native_file_dialog_path(fc, i));
-            if(archive_handler)
-            {
-                c = archive_handler->count_files(al_get_native_file_dialog_path(fc, i));
-                for(j = 0; j < c; j++)
-                {
-                    player = omo_get_player(app->player_registry, archive_handler->get_file(al_get_native_file_dialog_path(fc, i), j));
-                    if(player)
-                    {
-                        sprintf(buf, "%d", j);
-                        omo_add_file_to_queue(app->queue, al_get_native_file_dialog_path(fc, i),  buf);
-                    }
-                }
-            }
-
-            /* otherwise, add single file */
-            else
-            {
-                player = omo_get_player(app->player_registry, al_get_native_file_dialog_path(fc, i));
-                if(player)
-                {
-                    for(j = 0; j < player->get_track_count(al_get_native_file_dialog_path(fc, i)); j++)
-                    {
-                        sprintf(buf, "%d", j);
-                        omo_add_file_to_queue(app->queue, al_get_native_file_dialog_path(fc, i), buf);
-                    }
-                }
-            }
-        }
+        add_files_to_queue(fc, app->queue, data);
         app->queue_pos = -1;
     }
     al_destroy_native_file_dialog(fc);
@@ -193,9 +201,8 @@ int omo_menu_file_queue_files(void * data)
     APP_INSTANCE * app = (APP_INSTANCE *)data;
     ALLEGRO_FILECHOOSER * fc;
     OMO_QUEUE * new_queue;
-    OMO_ARCHIVE_HANDLER * archive_handler;
     int total_files = 0;
-    int i, j, c;
+    int i;
 
 	fc = al_create_native_file_dialog(app->last_music_filename, "Select music files.", omo_get_type_string(data), ALLEGRO_FILECHOOSER_FILE_MUST_EXIST | ALLEGRO_FILECHOOSER_MULTIPLE);
 	if(!fc)
@@ -224,22 +231,7 @@ int omo_menu_file_queue_files(void * data)
         }
     }
     omo_destroy_queue(app->queue);
-    for(i = 0; i < al_get_native_file_dialog_count(fc); i++)
-    {
-        archive_handler = omo_get_archive_handler(app->archive_handler_registry, al_get_native_file_dialog_path(fc, i));
-        if(archive_handler)
-        {
-            c = archive_handler->count_files(al_get_native_file_dialog_path(fc, i));
-            for(j = 0; j < c; j++)
-            {
-                omo_add_file_to_queue(new_queue, al_get_native_file_dialog_path(fc, i),  archive_handler->get_file(al_get_native_file_dialog_path(fc, i), j));
-            }
-        }
-        else if(omo_get_player(app->player_registry, al_get_native_file_dialog_path(fc, i)))
-        {
-            omo_add_file_to_queue(new_queue, al_get_native_file_dialog_path(fc, i), NULL);
-        }
-    }
+    add_files_to_queue(fc, new_queue, data);
     al_destroy_native_file_dialog(fc);
     app->queue = new_queue;
     return 1;
@@ -259,14 +251,9 @@ static int file_count = 0;
 
 static bool count_file(const char * fn, void * data)
 {
-    file_count++;
-    return false;
-}
-
-static bool process_file(const char * fn, void * data)
-{
     APP_INSTANCE * app = (APP_INSTANCE *)data;
     OMO_ARCHIVE_HANDLER * archive_handler;
+    OMO_PLAYER * player;
     int i, c;
 
     archive_handler = omo_get_archive_handler(app->archive_handler_registry, fn);
@@ -275,14 +262,61 @@ static bool process_file(const char * fn, void * data)
         c = archive_handler->count_files(fn);
         for(i = 0; i < c; i++)
         {
-            omo_add_file_to_queue(app->queue, fn, archive_handler->get_file(fn, i));
+            player = omo_get_player(app->player_registry, archive_handler->get_file(fn, i));
+            if(player)
+            {
+                file_count++;
+            }
         }
     }
-    else if(omo_get_player(app->player_registry, fn))
+    else
     {
-        if(omo_add_file_to_queue(app->queue, fn, NULL))
+        player = omo_get_player(app->player_registry, fn);
+        if(player)
         {
-            return true;
+            c = player->get_track_count(fn);
+            for(i = 0; i < c; i++)
+            {
+                file_count++;
+            }
+        }
+    }
+    return false;
+}
+
+static bool process_file(const char * fn, void * data)
+{
+    APP_INSTANCE * app = (APP_INSTANCE *)data;
+    OMO_ARCHIVE_HANDLER * archive_handler;
+    OMO_PLAYER * player;
+    char buf[32] = {0};
+    int i, c;
+
+    archive_handler = omo_get_archive_handler(app->archive_handler_registry, fn);
+    if(archive_handler)
+    {
+        c = archive_handler->count_files(fn);
+        for(i = 0; i < c; i++)
+        {
+            player = omo_get_player(app->player_registry, archive_handler->get_file(fn, i));
+            if(player)
+            {
+                sprintf(buf, "%d", i);
+                omo_add_file_to_queue(app->queue, fn, buf);
+            }
+        }
+    }
+    else
+    {
+        player = omo_get_player(app->player_registry, fn);
+        if(player)
+        {
+            c = player->get_track_count(fn);
+            for(i = 0; i < c; i++)
+            {
+                sprintf(buf, "%d", i);
+                omo_add_file_to_queue(app->queue, fn, c > 1 ? buf : NULL);
+            }
         }
     }
     return false;
