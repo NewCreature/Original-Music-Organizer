86534acaefbad3a916c998178f1fbba4b440830f
Todd Cope <xander4jesus@yahoo.com>
2017-08-09 15:33:04 -0500

    Organize GUI data. Make placeholders for tags dialog.

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,17 +1,27 @@
 #ifndef OMO_UI_H
 #define OMO_UI_H
 
+#define OMO_UI_MAX_TAGS         16
+#define OMO_UI_MAX_TAG_LENGTH 1024
+
 typedef struct
 {
+    /* main dialog */
     T3GUI_DIALOG * ui_dialog;
 	T3GUI_ELEMENT * ui_queue_list_box_element;
 	T3GUI_ELEMENT * ui_queue_list_element;
 	T3GUI_ELEMENT * ui_button_element[6];
-	T3GUI_THEME * ui_box_theme;
+    int mode;
+
+    /* tags dialog */
+    T3GUI_DIALOG * tags_dialog;
+    char tags_text[OMO_UI_MAX_TAGS][OMO_UI_MAX_TAG_LENGTH];
+
+    /* theme data */
+    T3GUI_THEME * ui_box_theme;
 	T3GUI_THEME * ui_list_box_theme;
 	T3GUI_THEME * ui_button_theme;
 	char ui_button_text[6][8];
-    int mode;
 
 } OMO_UI;
 
