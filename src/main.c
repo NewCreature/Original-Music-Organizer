cac6822f085be6d3f3a4b748a30c87996e6bd119
Todd Cope <xander4jesus@yahoo.com>
2017-08-13 20:50:00 -0500

    Don't process logic for the main window with the tags window is open.

--- src/main.c
+++ src/main.c
@@ -69,131 +69,134 @@ void app_logic(void * data)
 		{
 			omo_file_chooser_logic(data);
 			t3gui_logic();
-			if(app->ui->ui_queue_list_element->id1 >= 0)
+			if(!app->ui->tags_display)
 			{
+				if(app->player->queue && app->ui->ui_queue_list_element->id1 >= 0)
+				{
+					if(app->player->state == OMO_PLAYER_STATE_PLAYING)
+					{
+						app->player->queue_pos = app->ui->ui_queue_list_element->id1 - 1;
+						omo_play_next_song(app->player);
+					}
+					else
+					{
+						app->player->queue_pos = app->ui->ui_queue_list_element->id1;
+						omo_start_player(app->player);
+					}
+					app->ui->ui_queue_list_element->id1 = -1;
+				}
+				sprintf(app->ui->ui_button_text[0], "|<");
 				if(app->player->state == OMO_PLAYER_STATE_PLAYING)
 				{
-					app->player->queue_pos = app->ui->ui_queue_list_element->id1 - 1;
-					omo_play_next_song(app->player);
+					sprintf(app->ui->ui_button_text[1], "||");
 				}
 				else
 				{
-					app->player->queue_pos = app->ui->ui_queue_list_element->id1;
-					omo_start_player(app->player);
+					sprintf(app->ui->ui_button_text[1], ">");
 				}
-				app->ui->ui_queue_list_element->id1 = -1;
-			}
-			sprintf(app->ui->ui_button_text[0], "|<");
-			if(app->player->state == OMO_PLAYER_STATE_PLAYING)
-			{
-				sprintf(app->ui->ui_button_text[1], "||");
-			}
-			else
-			{
-				sprintf(app->ui->ui_button_text[1], ">");
-			}
-			sprintf(app->ui->ui_button_text[2], "[]");
-			sprintf(app->ui->ui_button_text[3], ">|");
-			sprintf(app->ui->ui_button_text[4], "^");
-			sprintf(app->ui->ui_button_text[5], "+");
-			if(t3f_key[ALLEGRO_KEY_T])
-			{
-				omo_open_tags_dialog(app->ui, app);
-				t3f_key[ALLEGRO_KEY_T] = 0;
-			}
-			if(t3f_key[ALLEGRO_KEY_LEFT])
-			{
-				app->button_pressed = 0;
-				t3f_key[ALLEGRO_KEY_LEFT] = 0;
-			}
-			if(t3f_key[ALLEGRO_KEY_ENTER])
-			{
-				if(app->player->queue_pos != app->ui->ui_queue_list_element->d1)
+				sprintf(app->ui->ui_button_text[2], "[]");
+				sprintf(app->ui->ui_button_text[3], ">|");
+				sprintf(app->ui->ui_button_text[4], "^");
+				sprintf(app->ui->ui_button_text[5], "+");
+				if(t3f_key[ALLEGRO_KEY_T])
 				{
-					omo_stop_player(app->player);
-					app->player->queue_pos = app->ui->ui_queue_list_element->d1;
-					omo_start_player(app->player);
+					omo_open_tags_dialog(app->ui, app);
+					t3f_key[ALLEGRO_KEY_T] = 0;
 				}
-			}
-			if(t3f_key[ALLEGRO_KEY_RIGHT])
-			{
-				app->button_pressed = 3;
-				t3f_key[ALLEGRO_KEY_RIGHT] = 0;
-			}
-			switch(app->button_pressed)
-			{
-				case 0:
+				if(t3f_key[ALLEGRO_KEY_LEFT])
 				{
-					omo_play_previous_song(app->player);
-					break;
+					app->button_pressed = 0;
+					t3f_key[ALLEGRO_KEY_LEFT] = 0;
 				}
-				case 1:
+				if(t3f_key[ALLEGRO_KEY_ENTER])
 				{
-					switch(app->player->state)
+					if(app->player->queue_pos != app->ui->ui_queue_list_element->d1)
 					{
-						case OMO_PLAYER_STATE_STOPPED:
+						omo_stop_player(app->player);
+						app->player->queue_pos = app->ui->ui_queue_list_element->d1;
+						omo_start_player(app->player);
+					}
+				}
+				if(t3f_key[ALLEGRO_KEY_RIGHT])
+				{
+					app->button_pressed = 3;
+					t3f_key[ALLEGRO_KEY_RIGHT] = 0;
+				}
+				switch(app->button_pressed)
+				{
+					case 0:
+					{
+						omo_play_previous_song(app->player);
+						break;
+					}
+					case 1:
+					{
+						switch(app->player->state)
 						{
-							app->player->queue_pos = app->ui->ui_queue_list_element->d1;
-							omo_start_player(app->player);
-							break;
+							case OMO_PLAYER_STATE_STOPPED:
+							{
+								app->player->queue_pos = app->ui->ui_queue_list_element->d1;
+								omo_start_player(app->player);
+								break;
+							}
+							case OMO_PLAYER_STATE_PLAYING:
+							{
+								omo_pause_player(app->player);
+								break;
+							}
+							case OMO_PLAYER_STATE_PAUSED:
+							{
+								omo_resume_player(app->player);
+								break;
+							}
 						}
-						case OMO_PLAYER_STATE_PLAYING:
+						break;
+					}
+					case 2:
+					{
+						omo_stop_player(app->player);
+						break;
+					}
+					case 3:
+					{
+						omo_play_next_song(app->player);
+						break;
+					}
+					case 4:
+					{
+						if(OMO_KEY_CTRL)
 						{
-							omo_pause_player(app->player);
-							break;
+							omo_menu_file_play_folder(data);
+							t3f_key[ALLEGRO_KEY_COMMAND] = 0;
+							t3f_key[ALLEGRO_KEY_LCTRL] = 0;
+							t3f_key[ALLEGRO_KEY_RCTRL] = 0;
 						}
-						case OMO_PLAYER_STATE_PAUSED:
+						else
 						{
-							omo_resume_player(app->player);
-							break;
+							omo_menu_file_play_files(data);
 						}
+						break;
 					}
-					break;
-				}
-				case 2:
-				{
-					omo_stop_player(app->player);
-					break;
-				}
-				case 3:
-				{
-					omo_play_next_song(app->player);
-					break;
-				}
-				case 4:
-				{
-					if(OMO_KEY_CTRL)
+					case 5:
 					{
-						omo_menu_file_play_folder(data);
-						t3f_key[ALLEGRO_KEY_COMMAND] = 0;
-						t3f_key[ALLEGRO_KEY_LCTRL] = 0;
-						t3f_key[ALLEGRO_KEY_RCTRL] = 0;
+						if(OMO_KEY_CTRL)
+						{
+							omo_menu_file_queue_folder(data);
+							t3f_key[ALLEGRO_KEY_COMMAND] = 0;
+							t3f_key[ALLEGRO_KEY_LCTRL] = 0;
+							t3f_key[ALLEGRO_KEY_RCTRL] = 0;
+						}
+						else
+						{
+							omo_menu_file_queue_files(data);
+						}
+						break;
 					}
-					else
-					{
-						omo_menu_file_play_files(data);
-					}
-					break;
 				}
-				case 5:
-				{
-					if(OMO_KEY_CTRL)
-					{
-						omo_menu_file_queue_folder(data);
-						t3f_key[ALLEGRO_KEY_COMMAND] = 0;
-						t3f_key[ALLEGRO_KEY_LCTRL] = 0;
-						t3f_key[ALLEGRO_KEY_RCTRL] = 0;
-					}
-					else
-					{
-						omo_menu_file_queue_files(data);
-					}
-					break;
-				}
+				app->button_pressed = -1;
+				omo_player_logic(app->player, app->archive_handler_registry, app->codec_handler_registry);
+				break;
 			}
-			app->button_pressed = -1;
-			omo_player_logic(app->player, app->archive_handler_registry, app->codec_handler_registry);
-			break;
 		}
 	}
 }
