54fc58cdd89e9f224831a7b8c1e25fec0fe16b68
Todd Cope <xander4jesus@yahoo.com>
2017-08-16 11:43:56 -0500

    Add support for All->Unknown library combination.

--- src/library.c
+++ src/library.c
@@ -386,6 +386,32 @@ bool omo_get_library_song_list(OMO_LIBRARY * lp, const char * artist, const char
                 library_sort_by_title(lp);
             }
         }
+        else if(!strcmp(album, "Unknown"))
+        {
+            lp->song_entry = malloc(sizeof(unsigned long) * lp->entry_count);
+            lp->song_entry_count = 0;
+            if(lp->song_entry)
+            {
+                for(i = 0; i < lp->entry_count; i++)
+                {
+                    val = al_get_config_value(lp->entry_database, lp->entry[i]->id, "Album");
+                    if(val)
+                    {
+                        if(!strcmp(val, "Unknown"))
+                        {
+                            lp->song_entry[lp->song_entry_count] = i;
+                            lp->song_entry_count++;
+                        }
+                    }
+                    else
+                    {
+                        lp->song_entry[lp->song_entry_count] = i;
+                        lp->song_entry_count++;
+                    }
+                }
+                library_sort_by_title(lp);
+            }
+        }
     }
     return true;
 }
