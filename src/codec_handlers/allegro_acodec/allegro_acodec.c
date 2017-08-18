da05b1c2b62adc7622d954f9d07bd7d75ee81cd5
Todd Cope <xander4jesus@yahoo.com>
2017-08-18 18:59:29 -0500

    Add support for getting tags from Ogg Vorbis files in Allegro acodec codec handler.

--- src/codec_handlers/allegro_acodec/allegro_acodec.c
+++ src/codec_handlers/allegro_acodec/allegro_acodec.c
@@ -1,15 +1,78 @@
 #include "t3f/t3f.h"
 #include "t3f/music.h"
 
+#include <vorbis/vorbisfile.h>
+
 #include "../codec_handler.h"
 
 static char player_filename[1024] = {0};
 static OMO_CODEC_HANDLER codec_handler;
+static char codec_file_extension[16] = {0};
+static char codec_tag_buffer[1024] = {0};
 
 static bool codec_load_file(const char * fn, const char * subfn)
 {
+	ALLEGRO_PATH * path;
+
+	strcpy(codec_file_extension, "");
+	path = al_create_path(fn);
+	if(path)
+	{
+		strcpy(codec_file_extension, al_get_path_extension(path));
+		al_destroy_path(path);
+	}
 	strcpy(player_filename, fn);
 	return true;
+}
+
+static const char * codec_get_tag(const char * name)
+{
+	if(!strcasecmp(codec_file_extension, ".ogg"))
+	{
+		OggVorbis_File vf;
+		vorbis_comment * vf_comment;
+		char * vf_comment_text = NULL;
+		char tag_name[32] = {0};
+
+		if(!ov_fopen(player_filename, &vf))
+		{
+			vf_comment = ov_comment(&vf, -1);
+			if(vf_comment)
+			{
+				if(!strcmp(name, "Disc"))
+				{
+					strcpy(tag_name, "DISCNUMBER");
+				}
+				else if(!strcmp(name, "Track"))
+				{
+					strcpy(tag_name, "TRACKNUMBER");
+				}
+				else if(!strcmp(name, "Year"))
+				{
+					strcpy(tag_name, "DATE");
+				}
+				else if(!strcmp(name, "Comment"))
+				{
+					strcpy(tag_name, "DESCRIPTION");
+				}
+				else
+				{
+					strcpy(tag_name, name);
+				}
+				vf_comment_text = vorbis_comment_query(vf_comment, tag_name, 0);
+				if(vf_comment_text)
+				{
+					strcpy(codec_tag_buffer, vf_comment_text);
+				}
+			}
+			ov_clear(&vf);
+			if(vf_comment_text)
+			{
+				return codec_tag_buffer;
+			}
+		}
+	}
+	return NULL;
 }
 
 static int codec_get_track_count(const char * fn)
@@ -63,6 +126,7 @@ OMO_CODEC_HANDLER * omo_codec_allegro_acodec_get_codec_handler(void)
 	memset(&codec_handler, 0, sizeof(OMO_CODEC_HANDLER));
 	codec_handler.initialize = NULL;
 	codec_handler.load_file = codec_load_file;
+	codec_handler.get_tag = codec_get_tag;
 	codec_handler.get_track_count = codec_get_track_count;
 	codec_handler.play = codec_play;
 	codec_handler.pause = codec_pause;
