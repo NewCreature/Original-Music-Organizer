a2eb05d4329c1a5ef31118bb57e3e643aab759bc
Todd Cope <xander4jesus@yahoo.com>
2017-08-26 14:24:35 -0500

    Add theme module.

---
+++ src/theme.c
@@ -0,0 +1,107 @@
+#include "t3f/t3f.h"
+#include "t3gui/theme.h"
+#include "theme.h"
+
+OMO_THEME * omo_load_theme(const char * fn, int mode)
+{
+    OMO_THEME * tp;
+    const char * val;
+    char buf[32];
+    ALLEGRO_PATH * path = NULL;
+    int i;
+
+    tp = malloc(sizeof(OMO_THEME));
+    if(tp)
+    {
+        memset(tp, 0, sizeof(OMO_THEME));
+        tp->config = al_load_config_file(fn);
+        if(tp->config)
+        {
+            path = al_create_path(fn);
+            if(path)
+            {
+                if(mode == 0)
+                {
+                    for(i = 0; i < OMO_THEME_MAX_BITMAPS; i++)
+                    {
+                        sprintf(buf, "bitmap_%d", i);
+                        val = al_get_config_value(tp->config, "Settings", buf);
+                        if(val)
+                        {
+                            al_set_path_filename(path, val);
+                            t3f_load_resource((void **)(&tp->bitmap[i]), T3F_RESOURCE_TYPE_BITMAP, al_path_cstr(path, '/'), 0, 0, 0);
+                        }
+                    }
+                }
+                for(i = 0; i < OMO_THEME_MAX_GUI_THEMES; i++)
+                {
+                    sprintf(buf, "theme_%d", i);
+                    val = al_get_config_value(tp->config, "Settings", buf);
+                    if(val)
+                    {
+                        al_set_path_filename(path, val);
+                        tp->gui_theme[i] = t3gui_load_theme(al_path_cstr(path, '/'));
+                    }
+                }
+                for(i = 0; i < OMO_THEME_MAX_TEXTS; i++)
+                {
+                    strcpy(tp->text[i], "");
+                    sprintf(buf, "text_%d", i);
+                    val = al_get_config_value(tp->config, "Settings", buf);
+                    if(val)
+                    {
+                        strcpy(tp->text[i], val);
+                    }
+                }
+                al_destroy_path(path);
+            }
+            else
+            {
+                goto fail;
+            }
+        }
+        else
+        {
+            goto fail;
+        }
+    }
+    return tp;
+
+    fail:
+    {
+        if(path)
+        {
+            al_destroy_path(path);
+        }
+        if(tp->config)
+        {
+            al_destroy_config(tp->config);
+        }
+        if(tp)
+        {
+            free(tp);
+        }
+    }
+    return NULL;
+}
+
+void omo_destroy_theme(OMO_THEME * tp)
+{
+    int i;
+
+    for(i = 0; i < OMO_THEME_MAX_GUI_THEMES; i++)
+    {
+        if(tp->gui_theme[i])
+        {
+            t3gui_destroy_theme(tp->gui_theme[i]);
+        }
+    }
+    for(i = 0; i < OMO_THEME_MAX_BITMAPS; i++)
+    {
+        if(tp->bitmap[i])
+        {
+            t3f_destroy_resource(tp->bitmap[i]);
+        }
+    }
+    al_destroy_config(tp->config);
+}
---
+++ src/theme.h
@@ -0,0 +1,37 @@
+#ifndef OMO_THEME_H
+#define OMO_THEME_H
+
+#include "t3f/t3f.h"
+#include "t3gui/theme.h"
+
+#define OMO_THEME_MAX_BITMAPS     32
+#define OMO_THEME_MAX_GUI_THEMES  16
+#define OMO_THEME_MAX_TEXTS       32
+#define OMO_THEME_MAX_TEXT_LENGTH 32
+
+#define OMO_THEME_BITMAP_PREVIOUS_TRACK 0
+#define OMO_THEME_BITMAP_PLAY           1
+#define OMO_THEME_BITMAP_PAUSE          2
+#define OMO_THEME_BITMAP_STOP           3
+#define OMO_THEME_BITMAP_NEXT_TRACK     4
+#define OMO_THEME_BITMAP_OPEN           5
+#define OMO_THEME_BITMAP_ADD            6
+
+#define OMO_THEME_GUI_THEME_BOX         0
+#define OMO_THEME_GUI_THEME_LIST_BOX    1
+#define OMO_THEME_GUI_THEME_BUTTON      2
+
+typedef struct
+{
+
+    ALLEGRO_CONFIG * config;
+    ALLEGRO_BITMAP * bitmap[OMO_THEME_MAX_BITMAPS];
+    T3GUI_THEME * gui_theme[OMO_THEME_MAX_GUI_THEMES];
+    char text[OMO_THEME_MAX_TEXTS][OMO_THEME_MAX_TEXT_LENGTH];
+
+} OMO_THEME;
+
+OMO_THEME * omo_load_theme(const char * fn, int mode);
+void omo_destroy_theme(OMO_THEME * tp);
+
+#endif
