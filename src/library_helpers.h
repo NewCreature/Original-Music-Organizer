4dc6e5f37e9fb65ac2d92c2299d9e784a8e4d24d
Todd Cope <xander4jesus@yahoo.com>
2017-11-05 21:11:16 -0600

    Add missing include.

--- src/library_helpers.h
+++ src/library_helpers.h
@@ -1,6 +1,8 @@
 #ifndef OMO_LIBRARY_HELPERS_H
 #define OMO_LIBRARY_HELPERS_H
 
+#include "instance.h"
+
 void omo_cancel_library_setup(APP_INSTANCE * app);
 void omo_setup_library(APP_INSTANCE * app, const char * file_database_fn, const char * entry_database_fn, ALLEGRO_CONFIG * config);
 const char * omo_get_library_file_id(OMO_LIBRARY * lp, const char * fn, const char * subfn, const char * track);
