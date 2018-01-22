a4a90847158591485f123d7697a18a4293d0a08a
Todd Cope <xander4jesus@yahoo.com>
2018-01-22 17:17:51 -0600

    Add functions for settings up and removing profiles.

--- src/profile.c
+++ src/profile.c
@@ -1,4 +1,5 @@
 #include "t3f/t3f.h"
+#include "t3f/file_utils.h"
 
 const char * omo_get_profile_section(char * buffer)
 {
@@ -14,4 +15,63 @@ const char * omo_get_profile_section(char * buffer)
 		strcpy(buffer, "Profile Default");
 	}
 	return buffer;
+}
+
+bool omo_setup_profile(const char * name)
+{
+	ALLEGRO_PATH * path;
+
+	path = al_clone_path(t3f_data_path);
+	if(path)
+	{
+		al_append_path_component(path, "Profiles");
+		al_append_path_component(path, name);
+		if(al_make_directory(al_path_cstr(path, '/')))
+		{
+			al_destroy_path(path);
+			return true;
+		}
+		al_destroy_path(path);
+	}
+
+	return false;
+}
+
+void omo_remove_profile(const char * name)
+{
+	char buffer[1024];
+	char profile_path[1024];
+	char buf[64];
+	int i, j, c;
+	const char * val;
+
+	val = al_get_config_value(t3f_config, "Settings", "profiles");
+	if(val)
+	{
+		c = atoi(val);
+		for(i = 0; i < c; i++)
+		{
+			sprintf(buf, "profile_%d", i);
+			val = al_get_config_value(t3f_config, "Settings", buf);
+			if(val)
+			{
+				if(!strcmp(val, name))
+				{
+					for(j = i + 1; j < c; j++)
+					{
+						sprintf(buf, "profile_%d", j);
+						val = al_get_config_value(t3f_config, "Settings", buf);
+						sprintf(buf, "profile_%d", i);
+						al_set_config_value(t3f_config, "Settings", buf, val ? val : "");
+						i++;
+					}
+					sprintf(buf, "%d", c - 1 < 0 ? 0 : c - 1);
+					al_set_config_value(t3f_config, "Settings", "profiles", buf);
+					break;
+				}
+			}
+		}
+	}
+	sprintf(profile_path, "%s/%s", t3f_get_filename(t3f_data_path, "profiles", buffer, 1024), name);
+	t3f_remove_directory(profile_path);
 }
--- src/profile.h
+++ src/profile.h
@@ -2,5 +2,7 @@
 #define OMO_PROFILE_H
 
 const char * omo_get_profile_section(char * buffer);
+bool omo_setup_profile(const char * name);
+void omo_remove_profile(const char * name);
 
 #endif
