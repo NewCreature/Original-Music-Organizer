1cfbff264a9b051db320d68d292cd3ec00ebf021
Todd Cope <todd@t3-i.com>
2023-03-24 18:30:28 -0500

    Fix construction of multi-tags dialog.

--- src/ui/multi_tags_dialog.c
+++ src/ui/multi_tags_dialog.c
@@ -27,7 +27,7 @@ bool omo_open_multi_tags_dialog(OMO_UI * uip, void * data)
 		}
 	}
 	h = al_get_font_line_height(uip->main_theme->gui_theme[OMO_THEME_GUI_THEME_LIST_BOX]->state[0].font[0]) * 3 + 4;
-	h *= (rows / 2 + (rows % 2 == 0 ? 1 : 0));
+	h *= (rows / 2 + 1);
 	h += 8;
 
 	val = al_get_config_value(t3f_config, "Settings", "theme");
@@ -44,7 +44,7 @@ bool omo_open_multi_tags_dialog(OMO_UI * uip, void * data)
 			if(uip->tag_enabled[i])
 			{
 				row++;
-				if(row > rows / 2)
+				if(row > rows / 2 + rows % 2)
 				{
 					row = 0;
 					column = 1;
