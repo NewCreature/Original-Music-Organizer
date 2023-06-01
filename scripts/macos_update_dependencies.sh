ace43422eeb953809b593e97e38abdc743457186
Todd Cope <todd@t3-i.com>
2023-06-01 14:15:38 -0500

    Use 'X86_SDK' and 'ARM_SDK' variables to simplify maintenance.

--- scripts/macos_update_dependencies.sh
+++ scripts/macos_update_dependencies.sh
@@ -16,12 +16,14 @@ if [ "$#" -ne 1 ]; then
 fi
 
 START_PATH=$(pwd)
+X86_SDK=MacOSX10.13.sdk
+ARM_SDK=MacOSX11.sdk
 
 mkdir -p $1
 cd $1
 
 # libbinio
-SDK_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX10.13.sdk
+SDK_PATH=/Library/Developer/CommandLineTools/SDKs/$X86_SDK
 if [ ! -d "libbinio" ];
 then
   git clone https://github.com/NewCreature/libbinio.git
@@ -33,7 +35,7 @@ sudo make -f makefile.macos install
 cd ..
 
 # adplug
-SDK_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX10.13.sdk
+SDK_PATH=/Library/Developer/CommandLineTools/SDKs/$X86_SDK
 if [ ! -d "adplug" ];
 then
   git clone https://github.com/NewCreature/adplug.git
@@ -45,7 +47,7 @@ sudo make -f makefile.macos install
 cd ..
 
 # libgme
-SDK_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX10.13.sdk
+SDK_PATH=/Library/Developer/CommandLineTools/SDKs/$X86_SDK
 if [ ! -d "libgme" ];
 then
   git clone https://github.com/NewCreature/libgme.git
@@ -57,7 +59,7 @@ cd _build_x86
 cmake .. -DCMAKE_OSX_SYSROOT=$SDK_PATH -DCMAKE_OSX_ARCHITECTURES=i386\;x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.6
 make
 cd ..
-SDK_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX11.sdk
+SDK_PATH=/Library/Developer/CommandLineTools/SDKs/$ARM_SDK
 remake_dir _build_arm
 cd _build_arm
 cmake .. -DCMAKE_OSX_SYSROOT=$SDK_PATH -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0
@@ -74,13 +76,13 @@ then
 fi
 cd mpg123
 git pull
-./configure CFLAGS="-arch i386 -arch x86_64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX10.13.sdk -mmacos-version-min=10.6" --enable-shared=no --enable-static=yes --with-cpu=generic
+./configure CFLAGS="-arch i386 -arch x86_64 -isysroot /Library/Developer/CommandLineTools/SDKs/$X86_SDK -mmacos-version-min=10.6" --enable-shared=no --enable-static=yes --with-cpu=generic
 make
 mv src/libmpg123/.libs/libmpg123.a libmpg123.a
 make clean
 echo ---------------------------------
 echo ---------------------------------
-./configure CFLAGS="-arch arm64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX11.sdk -mmacos-version-min=11.0" --enable-shared=no --enable-static=yes --with-cpu=generic --host=`uname -m`-apple-darwin
+./configure CFLAGS="-arch arm64 -isysroot /Library/Developer/CommandLineTools/SDKs/$ARM_SDK -mmacos-version-min=11.0" --enable-shared=no --enable-static=yes --with-cpu=generic --host=`uname -m`-apple-darwin
 make
 merge_libs src/libmpg123/.libs . libmpg123.a
 sudo cp src/libmpg123/mpg123.h /usr/local/include
