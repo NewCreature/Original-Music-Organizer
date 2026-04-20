7fd026c0e393530a39fa61d077dab29acad1e85a
Todd Cope <todd@t3-i.com>
2026-04-20 10:53:19 -0500

    Check for held instead of pressed status to restore correct functionality.

--- src/ui/frontend/allegro/gui/menu_proc.c
+++ src/ui/frontend/allegro/gui/menu_proc.c
@@ -865,7 +865,7 @@ int omo_menu_view_basic(int id, void * data)
 		al_set_config_value(t3f_config, "Settings", "library_view_height", buf);
 		al_set_config_value(t3f_config, "Settings", "last_view", "basic");
 
-		if(!t3f_key_pressed(ALLEGRO_KEY_LCTRL) && !t3f_key_pressed(ALLEGRO_KEY_RCTRL) && !t3f_key_pressed(ALLEGRO_KEY_COMMAND) && v_x && v_y && v_width && v_height)
+		if(!t3f_key_held(ALLEGRO_KEY_LCTRL) && !t3f_key_held(ALLEGRO_KEY_RCTRL) && !t3f_key_held(ALLEGRO_KEY_COMMAND) && v_x && v_y && v_width && v_height)
 		{
 			c_x = atoi(v_x);
 			c_y = atoi(v_y);
